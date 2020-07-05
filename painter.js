53dccf11845ad88c9435c8f069f184d9a86157be
tschneider-at <67682598+tschneider-at@users.noreply.github.com>
2020-07-05 03:24:18 +0200

    Update

--- painter.js
+++ painter.js
@@ -5,11 +5,25 @@ canvas.height = window.innerHeight
 
 const context = canvas.getContext("2d")
 
-context.strokeStyle = "rgb(250, 250, 190)"
-context.lineWidth = 1
+context.strokeStyle = "#0000ee"
+context.lineWidth = 2
 context.lineCap = "round"
 
+let shouldPaint = true
+
 document.addEventListener("mousemove", function (event) {
-    context.lineTo(event.pageX, event.pageY)
-    context.stroke()
+    shouldPaint = true
+})
+
+document.addEventListener("click", function (event) {
+    shouldPaint = false
+})
+
+document.addEventListener("mousemove", function (event) {
+
+    if (shouldPaint) {
+        context.lineTo(event.pageX, event.pageY)
+        context.stroke()
+    }
+    
 })
