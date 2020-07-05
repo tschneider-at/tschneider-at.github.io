71602c65ce92243a05af2f0459c4c2b087d42b97
tschneider-at <67682598+tschneider-at@users.noreply.github.com>
2020-07-05 12:20:09 +0200

    update

--- all.js
+++ all.js
@@ -1,19 +1,3 @@
-function is_touch_device() {
-    var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
-    var mq = function (query) {
-        return window.matchMedia(query).matches;
-    }
-
-    if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-        return true;
-    }
-
-    // include the 'heartz' as a way to have a non matching MQ to help terminate the join
-    // https://git.io/vznFH
-    var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
-    return mq(query);
-}
-
 const canvas = document.querySelector("canvas")
 
 canvas.width = window.innerWidth
